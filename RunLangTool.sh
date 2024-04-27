85b13f911e4939b9b06e89be9368e512d35ed0bf
DrLynch <cflynch@ncsu.edu>
2024-04-27 14:38:16 -0400

    Adding run script and fixing local server needs.

---
+++ RunLangTool.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# ===============================
+# RunLangTool.sh
+# Collin F. Lynch
+#
+# This bash script provides a simple wrapper to run the 
+# learning observer service and pipe the data to a logfile
+# over time this should be integrated into the systemd 
+# service process.  This uses static variables to specify
+# the location of the virtualenv and the command and 
+# specifies the location for the running logfile. 
+
+# System Variables
+# --------------------------------------
+VIRTUALENV_PATH="/usr/local/share/projects/WritingObserver/VirtualENVs/WOvenv2"
+#VIRTUALENV_PYTHON="/usr/local/share/Projects/WritingObserver/VirtualENVs/learning_observer/bin/python3.9"
+LANGTOOL_LOC="/usr/local/share/projects/WritingObserver/Repositories2/AWE_LanguageTool/"
+LOGFILE_DEST="/usr/local/share/projects/WritingObserver/Repositories2/ArgLab_writing_observer/learning_observer/learning_observer/logs"
+
+# Make the logfile name
+# ---------------------------------------
+LOG_DATE=$(date "+%m-%d-%Y--%H-%M-%S")
+LOGFILE_NAME="$LOGFILE_DEST/language_tool_$LOG_DATE.log"
+echo $LOG_NAME;
+
+ 
+# Now run the thing.
+# --------------------------------------
+echo "Running Language Tool Service..."
+cd $LANGTOOL_LOC
+source $VIRTUALENV_PATH/bin/activate
+nohup python awe_languagetool/languagetoolServer.py > $LOGFILE_NAME 2>&1 &
+PROCESS_ID=$!
+echo $PROCESS_ID > $LOGFILE_DEST/langtoolrun.pid
+
+# Set the number of allowed open files to something large 8192
+prlimit --pid $PROCESS_ID --nofile=8192
